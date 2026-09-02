06ae1de1e05d9b37ff4e4b0ebcc6333b34b9dfdd
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-02 21:06:09 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,12 @@
+public class Day01{
+  public static void main(string[] args) {
+    // 1.komentar satu baris 
+  /*
+    2. kalau ini 
+      contoh komentar
+      banyak baris
+      */
+  /** 
+    * 3. Komentar dokumentasi
+    */
+  System.out.println("commit pertama saya");
