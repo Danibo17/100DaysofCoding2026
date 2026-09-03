ffe6273c28f66b0fb69ce6f6e80d9bedb99e5727
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-03 01:57:19 -0700

    Create Day02.java

---
+++ Day02.java
@@ -0,0 +1,20 @@
+public class day02 {
+public static void main(String[] args {
+
+    // buat biodata menggunakan println, print, printf
+
+    //println menampilkan teks dan langsung ganti baris
+    System.out.println("BIODATA DIRI");
+    System.out.println("Nama : Rahmadani");
+    System.out.println("Umur : 17 tahun");
+    System.out.println("Hobi : Badminton");
+
+    //print() menampilkan teks tanpa ganti baris
+    System.out.print("Prodi : ");
+    System.out.print("Teknik Informatika ");
+
+    //printf menampilkan format yang rapi
+    System.out.printf("%nAsal : %s%n", "Kabupaten Mamuju"):
+    System.out.printf("Jenis Kelamin : %s%n ", "Perempuan");
+  }
+}
