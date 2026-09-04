17a030c5e9754623dec41500daed7c850bd7a875
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-03 23:51:41 -0700

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,23 @@
+public class day03 {
+public static void main(String[] args {
+        
+        System.out.println("==== DATA DIRI MAHASISWA ====");
+        // pakai \n untuk pindah baris
+        System.out.print("Nama : Rahmadani\n");
+        // pakai \t untuk jarak tab
+        System.out.print("NIM : D0226003\t\n");
+        System.out.print("Prodi : Teknik Informatika\t\n");
+
+        // pakai \ untuk tanda kutip
+        System.out.println("Status : \"Mahasiswa Semester 1\"");
+
+        // pakai \\ untuk garis miring
+        System.out.println(" Jalur : SNBP \\ Seleksi Berdasarkan Prestasi");
+
+        // gabung print & println
+        System.out.print("Hobi : ");
+        System.out.println("Olahraga, Badminton");
+
+        System.out.println("===================");
+    }
+}
