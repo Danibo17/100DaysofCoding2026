924d588359cd8266ef02ae40ff8fda66f66270ca
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-06 21:13:04 -0700

    Create Day06.java

---
+++ Day06.java
@@ -0,0 +1,14 @@
+public class Day06 {
+    public static void main(String[] args) {
+        String nama = "Dani";
+        char grade = 'A';
+        boolean aktif = true;
+        System.out.println("=============================");
+        System.out.println("        DATA MAHASISWA");
+        System.out.println("=============================");
+        System.out.println("Nama : " + nama);
+        System.out.println("Grade : " + grade);
+        System.out.println("Aktif : " + aktif);
+        System.out.println("=============================");
+    }
+}
