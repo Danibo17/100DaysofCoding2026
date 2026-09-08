a48cd8b806b8e9ffb4afb41fef6dbe9f4adbc570
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-07 23:55:32 -0700

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,14 @@
+public class Day07 {
+    public static void main(String[] args ) {
+        // Deklarasi variabel tipe String
+        String nama = "Dani";
+        String jurusan = "Informatika";
+        String kampus = "Unsulbar";
+        // Menampilkan output
+        System.out.println("===== DATA MAHASISWA=====");
+        System.out.println("Nama\t: "+ nama);
+        System.out.println("Jurusan\t: "+ jurusan);
+        System.out.println("Kampus\t: "+ kampus);
+        System.out.println("=========================");
+    }
+}
