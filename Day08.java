d775be26396e986306667a8a4ced3eac0f6625f5
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-09 00:32:38 -0700

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,21 @@
+public class Day08 {
+    public static void main(String[] args) {
+        // 1. Deklarasi variabel : membuat variabel yang akan digunakan
+        int umur;
+        String nama;
+
+        // 2. Inisialisasi : memberikan nilai awal
+        umur = 17;
+        nama = "Dani";
+        System.out.println("Sebelum diubah:");
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+
+        // 3. Update (Ubah nilai)
+        umur = 18;
+        nama = "Dani Ibo";
+        System.out.println("\nSetelah diubah:");
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+    }
+}
