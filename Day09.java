271a8a9833ef4c017aa27b9437959ebb5e37de7e
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-09 22:07:58 -0700

    Update Day09.java

--- Day09.java
+++ Day09.java
@@ -1,11 +1,11 @@
-public class day09 {
+public class Day09 {
     public static void main(String[] args) {
         // Deklarasi konstanta, nilai tetap yang tidak dapat diubah
         final double HARGA = 15000;
         final double DISKON = 0.10; // 10%
         // Menghitung potongan dan harga akhir
         double potongan = HARGA * DISKON;
-        double hargaAkhir = HARGA - DISKON;
+        double hargaAkhir = HARGA - potongan;
         // Hasil
         System.out.println("Harga Awal : Rp" + HARGA);
         System.out.println("Diskon 10% : Rp" + potongan);
