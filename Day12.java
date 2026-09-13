dc3cd97d83e1f31b50ae3889aa147992d71d4b39
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-13 08:09:25 -0700

    Update Day12.java

--- Day12.java
+++ Day12.java
@@ -1,44 +1,71 @@
 import java.util.Scanner;
 public class Day12 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("=== INPUT BIODATA MAHASISWA ===");
+        Scanner input = new Scanner(System.in);  
+        // === INPUT DATA ===
+        System.out.print("Masukkan Username\t: ");
+        String username = input.nextLine();
+        System.out.print("Masukkan Umur\t\t: ");
+        byte umur = input.nextByte();
 
-        System.out.print("Masukkan nama         : ");
-        String nama = input.nextLine();
-
-        System.out.print("Masukkan nim          : ");
-        String nim = input.nextLine();
-
-        System.out.print("Masukkan kelas        : ");
-        String kelas = input.nextLine();
-
-        System.out.print("Masukkan jurusan      : ");
-        String jurusan = input.nextLine();
-
-        System.out.print("Masukkan umur         : ");
-        int umur = input.nextInt();
-
-        System.out.print("Masukkan tinggi badan : ");
+        System.out.print("Masukkan Tinggi Badan\t\t: ");
         double tinggi = input.nextDouble();
-        input.nextLine(); // bersihkan buffer
-
-        System.out.print("Masukkan hobi         : ");
-        String hobi = input.nextLine();
-
-        System.out.print("Masukkan grade        : ");
-        char grade = input.next().charAt(0);
-        
-        System.out.println("\n========== BIODATA MAHASISWA ==========");
-        System.out.printf("%-18s    : %s%n", "Nama Lengkap", nama);
-        System.out.printf("%-18s    : %s%n", "NIM", nim);
-        System.out.printf("%-18s    : %s%n", "Kelas", kelas);
-        System.out.printf("%-18s    : %s%n", "Jurusan", jurusan);
-        System.out.printf("%-18s    : %d tahun%n", "Umur", umur);
-        System.out.printf("%-18s    : %.2f cm%n", "Tinggi Badan", tinggi);
-        System.out.printf("%-18s    : %s%n", "Hobi", hobi);
-        System.out.printf("%-18s    : %c%n", "Grade", grade);
-        System.out.println("=========================================");
-        input.close();
+        System.out.print("Masukkan Gol.darah\t\t: ");
+        char golDarah = input.next().charAt(0);
+        System.out.print("Apakah Akun Privat?\t: ");
+        boolean status = input.nextBoolean();
+        // ===== TAMPILKAN PROFIL =====
+        System.out.println("\n=== PROFIL FACEGRAM ===");
+        System.out.println("Username\t: " + username);
+        System.out.println("Umur\t\t: " + umur + " Tahun");
+        System.out.println("Tinggi Badan\t: " + tinggi + " cm");
+        System.out.println("Gol. Darah\t: " + golDarah);
+        System.out.println("Akun Privat\t: " + status);
+        System.out.println("==========================");
     }
 }
+
+
+public class KasirKantin {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        final String NAMA_KANTIN = "KANTIN KEJUJURAN TI";
+        System.out.print("Nama Makanan\t: ");
+        String namaMakanan = input.nextLine();
+        System.out.print("Harga Satuan\t: ");
+        int harga = input.nextLine();
+        System.out.print("Jumlah Porsi\t ");
+        int jumlah = input.nextLine();
+
+        int total = harga * jumlah;
+        // === TAMPILKAN STRUK ===
+        System.out.println("\n===========================");
+        System.out.println("\t" + NAMA_KANTIN);
+        System.out.println("Makanan\t\t: " + namaMakanan);
+        System.out.println("Harga Satuan\t: Rp " + harga);
+        System.out.println("Jumlah Porsi\t: " + jumlah);
+        System.out.println("-----------------------------");
+        System.out.println("TOTAL BAYAR\t: Rp " + total);
+        System.out.println("-----------------------------");
+    }
+}
+
+
+public class Day12 {
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    // === INPUT ===
+    System.out.print("Masukkan Nama Mahasiswa\t: ");
+    String nama = input.nextLine();
+    System.out.print("Masukkan Nilai Ujian\t: ");
+    double nilai = input.nextDouble();
+
+    System.out.println("Data diterima: " + nama + " mendapat nilai " + nilai);
+    nilai = 0.0;
+    System.out.println("AWAS! SISTEM DIRETAS!");
+    System.out.println("Nilai akhir " + nama + " sekarang diubah menjadi: " + nilai);
+}
+}
+
+
