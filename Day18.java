6b817d17e5379f1c80ee2c78e1a83171cc7569ec
Danibo17 <rahmadani17kosong9@gmail.com>
2026-09-18 10:52:08 -0700

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,25 @@
+public class Day18 {
+    public static void main(String[] args) {
+        // Mulai dari tipe data terkecil: byte
+        byte nilaiByte = 10;
+        
+        short nilaiShort = nilaiByte;
+
+        int nilaiInt = nilaiShort;
+
+        long nilaiLong = nilaiInt;
+
+        float nilaiFloat = nilaiLong;
+
+        double nilaiDouble = nilaiFloat;
+
+        System.out.println("Byte        : " + nilaiByte);
+        System.out.println("Short       : " + nilaiShort);
+        System.out.println("Int         : " + nilaiInt);
+        System.out.println("Long        : " + nilaiLong);
+        System.out.println("Float       : " + nilaiFloat);
+        System.out.println("Double      : " + nilaiDouble);
+        
+
+    }
+}
